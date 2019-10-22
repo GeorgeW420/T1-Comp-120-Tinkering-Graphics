967c37a1c6d36499c6d85f00a70581a4c5997b98
GeorgeWilliams <gw218829@falmouth.ac.uk>
2019-10-22 11:02:20 +0100

    does not work, still a work in progress
    Signed-off-by: Morgan <MP232027@falmouth.ac.uk>

---
+++ Level Generation/levelGenerator.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+pygame.screen.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+x = 300
+y = 300
+radius = 200
+
+x = 370
+y = 350
+radius = 20
+
+x = 230
+y = 350
+radius = 20
+
+x = 300
+y = 280
+width = 120
+height = 100
+start_angle = 190
+end_angle = 350
+
+
